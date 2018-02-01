7edd1703ccfb3bb80eabb574d6952fc1eb5ccc93
Kentaro Watanabe <watanabe.kentaro.z01@as.ntt-tx.co.jp>
2018-02-01 14:27:40 +0900

    tool: change hooks update shell
    Change to update pre-commit in ".git" directory of each user.
    
    Signed-off-by: Kentaro Watanabe <watanabe.kentaro.z01@as.ntt-tx.co.jp>

--- git/cron/git_hooks_update.sh
+++ git/cron/git_hooks_update.sh
@@ -9,7 +9,8 @@ fi
 
 # work directory
 GIT_WORK_DIR=Soft-Patch-Panel_tools
-GIT_COPY_DIR=git/.git_template
+GIT_COPY_DIR=.git_template
+TARGET_USER_NAME=tx_*
 
 # git clone
 cd /tmp
@@ -22,14 +23,26 @@ then
 fi
 
 # Change mode
-find /tmp/${GIT_WORK_DIR}/${GIT_COPY_DIR} -type f | while read COPY_FILE
+find /tmp/${GIT_WORK_DIR}/git/${GIT_COPY_DIR} -type f | while read COPY_FILE
 do
 	chmod 755 ${COPY_FILE}
 done
 
 # Copy to user's directory
-ls -1d /home/tx_* | while read SETTING_USER
+ls -1d /home/${TARGET_USER_NAME} | while read SETTING_USER
 do
-	cp -rp /tmp/${GIT_WORK_DIR}/${GIT_COPY_DIR} ${SETTING_USER}/.
+	DEST_USER=`basename ${SETTING_USER}`
+	cp -rp /tmp/${GIT_WORK_DIR}/git/${GIT_COPY_DIR} ${SETTING_USER}/.
+	chown -R ${DEST_USER}:tx_group ${SETTING_USER}/${GIT_COPY_DIR}
 done
+
+# Copy to user's clone directory
+find /home/${TARGET_USER_NAME} -name ".git" -type d | while read GIT_CLONE_DIR
+do
+	DEST_USER=`echo ${GIT_CLONE_DIR} | sed -e "s#^/home/##" -e "s#/.*##"`
+	cp -rp /tmp/${GIT_WORK_DIR}/git/${GIT_COPY_DIR}/hooks ${GIT_CLONE_DIR}
+	chown -R ${DEST_USER}:tx_group ${GIT_CLONE_DIR}/hooks
+done
+
+# End
 rm -rf /tmp/${GIT_WORK_DIR}
